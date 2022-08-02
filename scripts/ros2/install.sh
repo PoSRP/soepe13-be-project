e0e03888832ef19bf7bbcc1365a2d18745af2a4d
Søren Riisom Pedersen <soepe13@student.sdu.dk>
2022-08-03 00:22:08 +0200

    Testing latex and ros2 workflows

---
+++ scripts/ros2/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -e
+
+apt-get update -q
+apt-get install -qy locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o \
+  /usr/share/keyrings/ros-archive-keyring.gpg &> /dev/null
+echo "deb [arch=$(dpkg --print-architecture) \
+  signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] \
+  http://packages.ros.org/ros2/ubuntu jammy main" | \
+  tee /etc/apt/sources.list.d/ros2.list &> /dev/null
+
+apt-get update -q
+apt-get install -qy ros-humble-desktop
+apt-get install -qy python3-colcon-ros python3-colcon-bash python3-colcon-zsh python3-rosdep
+
+exit 0
