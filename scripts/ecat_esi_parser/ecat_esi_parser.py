eae5b7ebf5fd2f9d2881f52314b721bdab085711
Søren Riisom Pedersen <soepe13@student.sdu.dk>
2022-08-02 23:47:34 +0200

    ecat_parser things

--- scripts/ecat_esi_parser/ecat_esi_parser.py
+++ scripts/ecat_esi_parser/ecat_esi_parser.py
@@ -10,17 +10,67 @@ import xml.etree.ElementTree as Xet
 from ecat_esi_classes import Description
 
 
+class Printer:
+    """ So I won't have to change each print statement """
+    def __init__(self, quiet: bool = False):
+        self.quiet = quiet
+
+    def __call__(self, s: str):
+        if not self.quiet:
+            print(s)
+
+
+printer: Printer = Printer()
+
+
+def object_to_yaml_recursive(obj, _content: typing.List[str] = None, _indent: int = 0) -> typing.List[str]:
+    """
+    Generating yaml formatted device description.
+    :param obj: Filled out Description or Description sub-object
+    :param _content: Content generated so far
+    :param _indent: Whitespace indentation for this level
+    :return: Yaml formatted device description as List[str]
+    """
+    """ Things we reject """
+    if callable(obj):
+        raise Exception('This object should not have a __call__ function')
+    if obj.__class__ in [str, list, dict, Xet.Element, None]:
+        raise Exception('Do not give me this type of object: ' + obj.__class__.__name__)
+    """ Yaml content """
+    if _content is None:
+        _content = list()
+    for key in obj.__dict__.keys():
+        if '_' in str(key):
+            continue
+        if obj.__dict__[key] is None:
+            continue
+        if isinstance(obj.__dict__[key], str):
+            line = ''.zfill(_indent).replace('0', ' ') + str(key) + ': "' + str(obj.__dict__[key]) + '"'
+            _content.append(line)
+        elif isinstance(obj.__dict__[key], list):
+            _content.append(''.zfill(_indent).replace('0', ' ') + str(key) + ':')
+            for item in obj.__dict__[key]:
+                if isinstance(item, str):
+                    _content.append(''.zfill(_indent + 2).replace('0', ' ') + str(key) +
+                                    ': "' + item + '"')
+                else:
+                    object_to_yaml_recursive(item, _content, _indent + 2)
+        else:
+            object_to_yaml_recursive(obj.__dict__[key], _content, _indent + 2)
+    """ Return something """
+    return _content
+
+
 def class_to_ros_msg_recursive(cls, store: dict) -> dict:
     """
     Converts a class to ROS message definition files.
-    It will attempt to recurse into class-level members until composed of nested strings.
+    It will attempt to recurse into class-level members until composed of basic types.
     It does so based on annotation information, so full annotation is required on non-standard types.
     :param cls: Root class to generate messages for
     :param store: Generated message storage as {file_name: file_content}
-    :return: The generated messages as {file_name: file_content}
+    :return: The generated messages as {file_name: file_content}, same object as parameter store
     """
-
-    """ Things we reject messages for """
+    """ Things we reject """
     if not callable(cls):
         raise Exception('This is not callable, which a raw class generally is .. ')
     if cls in [str, list, dict, Xet.Element]:
@@ -68,6 +118,7 @@ def class_to_ros_msg_recursive(cls, store: dict) -> dict:
         elif type_args[0] != str:
             class_to_ros_msg_recursive(type_args[0], store)
     """ Return """
+    printer('  generated ' + filename)
     return store
 
 
@@ -114,9 +165,8 @@ def get_files_in_path(esi_path: str, recurse=True) -> list:
     return files
 
 
-def install() -> None:
+def install(src_dir: str) -> None:
     ecat_interfaces_path = os.path.join(os.getcwd(), '..', '..', 'ros2', 'src', 'ecat_interfaces')
-    src_dir = os.path.join(os.getcwd(), 'generated-ros')
     """ Read msg and srv files in output directory """
     new_msgs = []
     new_srvs = []
@@ -130,9 +180,9 @@ def install() -> None:
         source = os.path.join(src_dir, new_file[1], new_file[0] + '.' + new_file[1])
         destination = os.path.join(ecat_interfaces_path, new_file[1], new_file[0] + '.' + new_file[1])
         if os.path.exists(destination):
-            print('  Overwriting file: ' + new_file[0] + '.' + new_file[1])
+            printer('  overwriting ' + new_file[0] + '.' + new_file[1])
         else:
-            print('  Copying new file: ' + new_file[0] + '.' + new_file[1])
+            printer('  creating ' + new_file[0] + '.' + new_file[1])
         shutil.copy(source, destination)
     """ Read and sort through contents of ecat_interfaces cmake """
     msg_dir = os.path.join(ecat_interfaces_path, 'msg')
@@ -148,27 +198,30 @@ def install() -> None:
               line.split('/')[1].split('.')[0],
               line.split('/')[1].split('.')[1]) for line in cmake_content[idx_start:idx_end]
              if ('msg/' in line and '.msg' in line) or ('srv/' in line and '.srv' in line)]
-    """ Add or remove msg and srv entries in cmake """
+    """ Remove current entries in cmake content """
     for line in lines:
         li = line[0] + '/' + line[1] + '.' + line[2]
         if cmake_content.count(li):
             cmake_content.remove(li)
+    """ Printing and adding new content """
     msgs = [os.path.basename(msg).split('.') for msg in get_files_in_path(msg_dir)]
     srvs = [os.path.basename(srv).split('.') for srv in get_files_in_path(srv_dir)]
     files = msgs + srvs
     for line in lines:
         if line[1] not in [file[0] for file in files]:
-            print('  Removing cmake entry: ' + line[1] + ('.msg' if line[2].find('.msg') else '.srv'))
+            printer('  Removing cmake entry: ' + line[1] + ('.msg' if line[2].find('.msg') else '.srv'))
     for file in files:
         if file[0] not in [line[1] for line in lines]:
-            print('  New cmake entry: ' + file[0] + '.' + file[1])
+            printer('  New cmake entry: ' + file[0] + '.' + file[1])
         str_line = ''.zfill(len(lines[0][0]) - len(lines[0][0].lstrip(' '))).replace('0', ' ') + \
                    '"' + file[1] + '/' + file[0] + '.' + file[1] + '"'
         cmake_content.insert(idx_start, str_line)
+    """ Sorting new content """
     idx_end = [cmake_content[idx_start:].index(line)+idx_start for line in cmake_content[idx_start:] if line == ')'][0]
     new_content = [line for line in cmake_content[idx_start:idx_end] if line.strip(' ') != '']
     new_content.sort()
     cmake_content[idx_start:idx_end] = new_content
+    """ Overwriting old cmake """
     os.remove(cmake_txt)
     with open(cmake_txt, 'w+') as f:
         f.write('\n'.join(cmake_content))
@@ -177,40 +230,45 @@ def install() -> None:
 def main():
     """
     Options:
-        -g, --generate <msg, description> ..
-            Generate ROS definition files
-        -p, --esi-path <path>
-            Override ESI directory path
         -f, --esi-file [file]
             Run on a single ESI file
-        -r, --recurse
-            Run recursively into the ESI directory when looking for ESI files
-            The default behaviour is to look only in the top level ESI directory
+        -g, --generate <msg, description> ..
+            Generate ROS definition files
+        -i, --install
+            Install generated files in the ecat_interfaces package
         -o, --output <path>
             Override definition file output directory
             Can only be used with -g
-        -i, --install
-            Install generated files in the ecat_interfaces package
-            If -g is not provided, the default output directory will be installed
-            If -g is provided, new output will be installed
+        -p, --esi-path <path>
+            Override ESI directory path
+        -q, --quiet
+            Run silently
+        -r, --recurse
+            Run recursively into the ESI directory when looking for ESI files
+            The default behaviour is to look only in the top level ESI directory
+        -t, --test
+            Run generate steps without generating output
+            Same as '-r -g msg description' with no output
     """
     parser = argparse.ArgumentParser(
-        description='Generating ROS message files and device descriptions from EtherCAT ESI files',
+        description='Generating ROS msg and srv files from EtherCAT ESI files',
         formatter_class=argparse.RawTextHelpFormatter)
 
     default_output_path = os.path.join(os.getcwd(), 'generated-ros')
     default_esi_path = os.path.join(os.getcwd(), 'device-descriptions')
+    default_esi_file = os.path.join(default_esi_path, 'elmo-ecat-desc-00010420.xml')
 
-    parser.add_argument('-f', '--file', default=None, nargs=1, type=str, dest='esi_file',
-                        help="ESI file to use")
+    parser.add_argument('-f', '--file', const=f'{default_esi_file}', nargs='?', default=None, type=str, dest='esi_file',
+                        help=f'ESI file to use'
+                             f'\n(default if no arg: {default_esi_file})')
 
     parser.add_argument('-g', '--generate', type=str, nargs='+', dest='generate', action='append',
                         choices=['msg', 'description'], default=None,
                         help='which output to generate')
 
     parser.add_argument('-i', '--install', action='store_true', dest='install',
-                        help='install generated files \n'
-                             'if used with -g new files are installed \n'
+                        help='install generated files\n'
+                             'if used with -g new files are installed\n'
                              '(default: install files from default directory)')
 
     parser.add_argument('-o', '--output', default=default_output_path, type=str, nargs=1, dest='output_path',
@@ -219,60 +277,66 @@ def main():
     parser.add_argument('-p', '--path', default=default_esi_path, type=str, dest='esi_path', nargs=1,
                         help="path to use containing only ESI files")
 
+    parser.add_argument('-q', '--quiet', action='store_true', dest='quiet',
+                        help='run silently')
+
     parser.add_argument('-r', '--recurse', action='store_true', dest='recurse',
                         help='recurse into ESI path (default: top-level only)')
 
+    parser.add_argument('-t', '--test', action='store_true', dest='test',
+                        help='test generators with no output')
+
     args = parser.parse_args()
+    printer.quiet = args.quiet
     esi_files = []
 
-    try:
-        if args.recurse and args.esi_file:
-            print('Cannot recurse over a single file, ignoring -r')
-            args.recurse = None
+    if args.recurse and args.esi_file:
+        printer('Cannot recurse over a single file, ignoring -r')
+        args.recurse = None
 
-        if args.recurse and not args.esi_file:
-            print('Recursing into ESI path: ' + args.esi_path)
-            esi_files = get_files_in_path(args.esi_path)
-            for f in esi_files:
-                print('    ' + os.path.basename(f))
-        elif not args.recurse and args.esi_file:
-            print('Using provided file: ' + args.esi_file)
-            esi_files.append(args.esi_file)
-        else:
-            print('Using top level ESI path: ' + args.esi_path)
-            esi_files = get_files_in_path(args.esi_path, recurse=False)
-            for f in esi_files:
-                print('    ' + os.path.basename(f))
+    if args.recurse and not args.esi_file:
+        printer('Recursing into ESI path: ' + args.esi_path)
+        esi_files = get_files_in_path(args.esi_path)
+        for f in esi_files:
+            printer('    ' + os.path.basename(f))
+    elif not args.recurse and args.esi_file:
+        printer('Using provided file: ' + args.esi_file)
+        esi_files.append(args.esi_file)
+    else:
+        printer('Using top level ESI path: ' + args.esi_path)
+        esi_files = get_files_in_path(args.esi_path, recurse=False)
+        for f in esi_files:
+            printer('    ' + os.path.basename(f))
 
-        if args.generate is not None:
-            if len(args.generate) > 1:
-                raise Exception('I did not know the args.generate list could have more than one entry')
+    if args.generate is not None:
+        if len(args.generate) > 1:
+            raise Exception('I did not know the args.generate list could have more than one entry')
 
-            if 'msg' in args.generate[0]:
-                print('Generating ROS message files')
-                if not os.path.isdir(os.path.join(args.output_path, 'msg')):
-                    os.makedirs(os.path.join(args.output_path, 'msg'))
-                generate_ros_msgs(os.path.join(args.output_path, 'msg'))
-                msgs = os.listdir(os.path.join(args.output_path, 'msg'))
-                for msg in msgs:
-                    print('  ' + msg)
+        if 'msg' in args.generate[0]:
+            printer('Generating ROS message files')
+            if not os.path.isdir(os.path.join(args.output_path, 'msg')):
+                os.makedirs(os.path.join(args.output_path, 'msg'))
+            generate_ros_msgs(os.path.join(args.output_path, 'msg'))
 
-            if 'description' in args.generate[0]:
-                print('Generating ROS device description files')
-                for esi_file in esi_files:
-                    print('    ' + os.path.basename(esi_file))
-                    device_description = Description(esi_file)
-                    generate_ros_device_description(device_description)
+        if 'description' in args.generate[0]:
+            printer('Generating ROS device description files')
+            for esi_file in esi_files:
+                printer('    ' + os.path.basename(esi_file))
+                device_description = Description(esi_file)
+                generate_ros_device_description(device_description)
 
-        if args.install:
-            print('Installing files into ecat_interfaces')
-            install()
+    if args.install:
+        printer('Installing files into ecat_interfaces')
+        install(args.output_path)
 
-        print('Done!')
-
-    except Exception as ex:
-        print('Exception during operation: ' + str(ex))
+    printer('Done!')
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+
+    default_esi_file = os.path.join(os.getcwd(), 'device-descriptions', 'elmo', 'elmo-ecat-desc-00010420.xml')
+    device = Description(default_esi_file)
+    device_yaml = object_to_yaml_recursive(device)
+    for yline in device_yaml:
+        print(yline)
