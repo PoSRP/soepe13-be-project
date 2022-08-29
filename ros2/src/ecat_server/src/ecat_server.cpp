ff6cd51fb17ebd5c51615fac2f604ba3169b0287
Søren Riisom Pedersen <soerenriisom@gmail.com>
2022-08-29 20:32:48 +0200

    Server basic action

--- ros2/src/ecat_server/src/ecat_server.cpp
+++ ros2/src/ecat_server/src/ecat_server.cpp
@@ -1,11 +1,140 @@
 #include "ecat_server/ecat_server.hpp"
 
-EcatServer::EcatServer(uint64_t pid)
-: _node_name("ecat_server_" + std::to_string(pid)), Node("ecat_server_" + std::to_string(pid))
+using namespace EtherCAT;
+
+Server::Server(const rclcpp::NodeOptions &options)
+: Node("ecat_server_" + std::to_string(getpid())),
+  _network_interface_param(""),
+  _network_cycle_time_us(1000),
+  _node_name("ecat_server_" + std::to_string(getpid())),
+  _execution_active(false)
 {
   using std::placeholders::_1;
   using std::placeholders::_2;
 
-  // Registering message publishers
-  _log_publisher = create_publisher<std_msgs::msg::String>("ecat_server/log", 10);
+  // Message topics
+  _log_publisher = create_publisher<std_msgs::msg::String>(_node_name + "/log", 10);
+
+  // Execute move action
+  _execute_move_action_server = rclcpp_action::create_server<ExecuteMove>(
+    get_node_base_interface(),
+    get_node_clock_interface(),
+    get_node_logging_interface(),
+    get_node_waitables_interface(),
+    "execute_move",
+    std::bind(&Server::_handle_execute_move_goal, this, _1, _2),
+    std::bind(&Server::_handle_execute_move_cancel, this, _1),
+    std::bind(&Server::_handle_execute_move_accepted, this, _1));
 }
+
+rclcpp_action::GoalResponse Server::_handle_execute_move_goal(
+	    const rclcpp_action::GoalUUID & uuid,
+	    std::shared_ptr<const ExecuteMove::Goal> goal)
+{
+	if (_execution_active.load()) {
+		RCLCPP_INFO(get_logger(), "Move request rejected, another move is active");
+		return rclcpp_action::GoalResponse::REJECT;
+	}
+
+	std::string data{};
+	for (std::size_t i = 0; i < goal->time_data.size(); i++)
+	{
+		data += "(" + std::to_string(goal->time_data.at(i)) +
+				", " + std::to_string(goal->value_data.at(i)) + ")";
+		if (i < goal->time_data.size() - 1)
+			data += "; ";
+	}
+
+	if (data.empty()) {
+		RCLCPP_INFO(get_logger(), "Move request rejected, no data provided");
+		return rclcpp_action::GoalResponse::REJECT;
+	}
+
+    RCLCPP_INFO(get_logger(), "Received move request with data: %s", data.c_str());
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse Server::_handle_execute_move_cancel(
+  const std::shared_ptr<ExecuteMoveGoalHandle> goal_handle)
+{
+    auto result = std::make_shared<ExecuteMove::Result>();
+	result->good = false;
+	result->msg = "Move aborted by cancel request";
+	RCLCPP_INFO(get_logger(), result->msg.c_str());
+    goal_handle->canceled(result);
+    return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void Server::_handle_execute_move_accepted(const std::shared_ptr<ExecuteMoveGoalHandle> goal_handle)
+{
+    using std::placeholders::_1;
+    // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+    std::thread{std::bind(&Server::_execute_move, this, _1), goal_handle}.detach();
+}
+
+void Server::_execute_move(const std::shared_ptr<ExecuteMoveGoalHandle> goal_handle)
+{
+	_execution_active.exchange(true);
+    RCLCPP_INFO(get_logger(), "Executing move");
+    rclcpp::Rate loop_rate(100);
+
+    const auto goal = goal_handle->get_goal();
+    const auto time_vec = goal->time_data;
+    const auto value_vec = goal->value_data;
+
+    auto feedback = std::make_shared<ExecuteMove::Feedback>();
+    auto &time_fb = feedback->fb_time_data;
+    auto &value_fb = feedback->fb_value_data;
+
+    auto result = std::make_shared<ExecuteMove::Result>();
+
+    /* TODO:
+     *  prepare OP thread buffers
+     * 	start network OP thread
+     * 	fill/read buffers in a timely manner
+     *  feedback read buffer
+     */
+    for (std::size_t i = 0; (i < time_vec.size()) && rclcpp::ok(); i++) {
+    	if (goal_handle->is_canceling()) {
+    		result->good = false;
+    		result->msg = "Move was cancelled";
+    		RCLCPP_INFO(get_logger(), result->msg.c_str());
+    		_execution_active.exchange(false);
+    		return;
+    	}
+
+    	time_fb.push_back(time_vec[i]);
+    	value_fb.push_back(value_vec[i]);
+    	goal_handle->publish_feedback(feedback);
+
+    	std::string data{};
+    	for (std::size_t i = 0; i < time_fb.size(); i++) {
+    		data += "(" + std::to_string(time_fb.at(i)) +
+    				", " + std::to_string(value_fb.at(i)) + ")";
+    		if (i < goal->time_data.size() - 1)
+    			data += "; ";
+    	}
+    	RCLCPP_INFO(get_logger(), "Sent feedback: %s", data.c_str());
+
+    	time_fb.clear();
+    	value_fb.clear();
+    	loop_rate.sleep();
+    }
+
+    if (rclcpp::ok()) {
+    	result->good = true;
+    	result->msg = "Move completed";
+		RCLCPP_INFO(get_logger(), result->msg.c_str());
+    }
+	_execution_active.exchange(false);
+	// TODO: Missing return in python?
+}
+
+
+
+
+
+
+
+
