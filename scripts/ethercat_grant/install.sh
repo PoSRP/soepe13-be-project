b7849f9bbbae4eaee7dcd782298b578416dcb592
srp-SHR <soerenriisom@gmail.com>
2022-09-11 16:01:43 +0200

    Library stuff

--- scripts/ethercat_grant/install.sh
+++ scripts/ethercat_grant/install.sh
@@ -6,29 +6,27 @@ launch_path=$(pwd)
 script_path=$(dirname $0)
 script_path=$(cd $script_path && pwd)
 lib_path=$script_path/../../lib
-shell_type=bash
 rcfile=/home/$USER/.bashrc
+shell_type=bash
 
 if [ -z $script_path ]; then printf "\nPATH ERROR: $script_path\n\n" && exit 1; fi
-if [[ ! -f /opt/ros/humble/setup.$shell_type ]]; then printf "\nNO ROS2 HUMBLE SETUP FILE FOUND: /opt/ros/humble/setup.$SHELL_TYPE\n\n" && exit 1; fi
+if [[ ! -f /opt/ros/humble/setup.$shell_type ]]; then printf "\nNO ROS2 HUMBLE SETUP FILE FOUND: /opt/ros/humble/setup.$shell_type\n\n" && exit 1; fi
 if [[ ! -f $rcfile ]]; then printf "\nNO RC FILE FOUND: $rcfile\n\n" && exit 2; fi
-
-source $rcfile
-source /opt/ros/humble/setup.$shell_type
 
 sudo apt-get install -qy libpcap-dev libcap-dev
 
 source $rcfile
 source /opt/ros/humble/setup.$shell_type
 
-if [ ! -d $lib_path ]; then mkdir $lib_path; fi
-cd $lib_path
-git clone https://github.com/PoSRP/ethercat_grant.git
-cd ethercat_grant
-git checkout --track origin/devel
+cd $lib_path/ethercat_grant
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ $current_branch == "devel" ]]; then git pull; else git checkout -b devel origin/devel; fi
+
+rm -rf build
 mkdir build
 cd build
+
 sudo ../scripts/preinst
 cmake ..
 sudo make install
--- scripts/soem/install.sh
+++ scripts/soem/install.sh
@@ -5,22 +5,28 @@ set -e
 launch_path=$(pwd)
 script_path=$(dirname $0)
 script_path=$(cd $script_path && pwd)
-roslib_path=$script_path/../../ros2/lib
-shell_type=bash
+lib_path=$script_path/../../ros2/lib
 rcfile=/home/$USER/.bashrc
+shell_type=bash
 
 if [ -z $script_path ]; then printf "\nPATH ERROR: $script_path\n\n" && exit 1; fi
-if [[ ! -f /opt/ros/humble/setup.$shell_type ]]; then printf "\nNO ROS2 HUMBLE SETUP FILE FOUND: /opt/ros/humble/setup.$SHELL_TYPE\n\n" && exit 1; fi
+if [[ ! -f /opt/ros/humble/setup.$shell_type ]]; then printf "\nNO ROS2 HUMBLE SETUP FILE FOUND: /opt/ros/humble/setup.$shell_type\n\n" && exit 1; fi
 if [[ ! -f $rcfile ]]; then printf "\nNO RC FILE FOUND: $rcfile\n\n" && exit 2; fi
 
 source $rcfile
 source /opt/ros/humble/setup.$shell_type
 
-if [ ! -d $roslib_path ]; then mkdir $roslib_path; fi
-cd $roslib_path
-git clone https://github.com/PoSRP/soem.git
-cd soem
-git checkout --track origin/devel
+cd $lib_path/soem
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [[ $current_branch == "devel" ]]; then git pull; else git checkout -b devel origin/devel; fi
+
+rm -rf build
+mkdir build
+cd build
+
+cmake ..
+sudo make install
 
 cd $launch_path
 exit 0
